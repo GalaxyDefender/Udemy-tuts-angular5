b0a89738648b36f7bf43e3d2cca03b7833309c5c
Quentin <qgerard.gerard@gmail.com>
2018-02-17 19:26:58 +0200

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showDetails = true;
+  log = [];
+
+  Btn() {
+    this.showDetails = !this.showDetails;
+    this.log.push(this.log.length + 1);
+  }
 }
